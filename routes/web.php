91ab64ccfdaa9ea125afd27eeae0aa3a85f00f1a
Chris Koivu <Cmkoivu@comcast.net>
2018-07-23 08:39:58 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,5 @@ Route::get('/admin', 'AdminController@admin')
     ->middleware('is_admin')    
     ->name('admin');
 
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
 
 
